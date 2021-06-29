60f7746336d25e47eded470ec2aa9d3bdeb77cdd
Sage Winn <sage.m.winn@gmail.com>
2021-06-29 16:12:46 -0400

    reworked git repository due to security risk

--- admin/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/cuts":["static\u002Fchunks\u002Fpages\u002Fcuts.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcuts"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
